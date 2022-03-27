7813b4736015564484b1f5034a5154053b549c2f
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-03-27 14:18:37 +0200

    style: Adjust boxShadow of Card

--- slide-deck/src/theme/components/Card.jsx
+++ slide-deck/src/theme/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ children, sx = {}, ...props }) => (
   <Block
     sx={{
       p: 2,
-      boxShadow: "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)",
+      boxShadow: "0 9px 18px rgba(0,0,0,0.30), 0 7px 6px rgba(0,0,0,0.22)",
       width: "100%",
       "& > *": { m: 0, p: 0 },
       ...sx,
