998986eb2a9431145caea2a32af507de75fa2c9b
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-03-27 14:13:18 +0200

    feat: Wrap with Zoom to keep aspect ratio

--- slide-deck/src/index.jsx
+++ slide-deck/src/index.jsx
@@ -4,31 +4,36 @@ import "./index.css";
 import { jsx } from "theme-ui";
 import { Fragment } from "react";
 import ReactDOM from "react-dom";
-import Deck from "@mdxp/core";
+import Deck, { Zoom } from "@mdxp/core";
 import * as components from "@mdxp/components";
 
 import Prism from "@theme-ui/prism";
 
 import theme from "./theme/theme.js";
 import themeComponents from "./theme/theme-components.js";
-
 import MDXPresentation from "./presentation.mdx";
 
 ReactDOM.render(
-  <Deck
-    components={{
-      ...components,
-      ...themeComponents,
-      code: Prism,
-      pre: ({ children }) => <Fragment>{children}</Fragment>,
-    }}
-    Layout={themeComponents.MDXPHeaderLayout}
-    layoutOptions={{ showSlideNum: true }}
-    theme={theme}
-    keyboardTarget={window}
+  <Zoom
+    maxWidth={1280}
+    width={1280}
+    aspectRatio={16 / 9}
+    sx={{ maxWidth: "100vw", maxHeight: "100vh" }}
   >
-    <MDXPresentation />
-  </Deck>,
-
+    <Deck
+      components={{
+        ...components,
+        ...themeComponents,
+        code: Prism,
+        pre: ({ children }) => <Fragment>{children}</Fragment>,
+      }}
+      Layout={themeComponents.MDXPHeaderLayout}
+      layoutOptions={{ showSlideNum: true }}
+      theme={theme}
+      keyboardTarget={window}
+    >
+      <MDXPresentation />
+    </Deck>
+  </Zoom>,
   document.getElementById("root")
 );
